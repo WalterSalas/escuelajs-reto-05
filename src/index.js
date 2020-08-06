d8c7183ceca40f610e84e40759d5fa00830990fd
Walter Salas <soy@waltersalas.com>
2020-08-06 16:06:55 -0500

    Third challenge solved

--- src/index.js
+++ src/index.js
@@ -1,45 +1,51 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://rickandmortyapi.com/api/character/";
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
+document.addEventListener("DOMContentLoaded", localStorage.clear());
 
-      const characters = response.results;
-      console.log(characters)
+const getData = (api) => {
+    if (localStorage.getItem("next_fetch") !== null) {
+        api = localStorage.getItem("next_fetch");
+    }
 
-      const next_fetch = localStorage.setItem('next_fetch', response.info.next);
-      const localData = localStorage.getItem('next_fetch');
-      console.log(localData)
+    fetch(api)
+        .then((response) => response.json())
+        .then((response) => {
+            const characters = response.results;
+            console.log(characters);
+            let output = characters
+                .map((character) => {
+                    return `
+                        <article class="Card">
+                            <img src="${character.image}" />
+                            <h2>${character.name}<span>${character.species}</span></h2>
+                        </article>`;
+                })
+                .join("");
+            let newItem = document.createElement("section");
+            newItem.classList.add("Items");
+            newItem.innerHTML = output;
+            $app.appendChild(newItem);
+            localStorage.setItem("next_fetch", response.info.next);
+            console.log(localStorage);
+        })
+        .catch((error) => console.log(error));
+};
 
-      let output = characters.map(character => {
-        return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
+const loadData = async () => {
+    await getData(API);
+};
 
-const loadData = () => {
-  getData(API);
-}
-
-const intersectionObserver = new IntersectionObserver(entries => {
-  if (entries[0].isIntersecting) {
-    loadData();
-  }
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const intersectionObserver = new IntersectionObserver(
+    (entries) => {
+        if (entries[0].isIntersecting) {
+            loadData();
+        }
+    },
+    {
+        rootMargin: "0px 0px 100% 0px",
+    }
+);
 
 intersectionObserver.observe($observe);
